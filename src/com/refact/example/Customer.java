a88d4a70d6d575bc4a8544a8ee2b19fb25de9c1a
florinoprea93 <florinoprea93@gmail.com>
2017-10-07 09:29:27 +0300

    Replace Temp with Query - extracting frequent renter points

--- src/com/refact/example/Customer.java
+++ src/com/refact/example/Customer.java
@@ -42,14 +42,8 @@ public class Customer {
         String result = "com.refact.example.Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            //add frecquent renter points
-            frequentRenterPoints++;
-            //add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRentalPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";

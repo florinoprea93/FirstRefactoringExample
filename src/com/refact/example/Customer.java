ead3de07b10036947ded88b80611ccc2b3264bc7
florinoprea93 <florinoprea93@gmail.com>
2017-10-07 09:13:13 +0300

    Replace Temp with Query - eliminate thisAmount

--- src/com/refact/example/Customer.java
+++ src/com/refact/example/Customer.java
@@ -45,9 +45,6 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            thisAmount = each.getCharge();
-
-
             //add frecquent renter points
             frequentRenterPoints++;
             //add bonus for a two day new release rental
@@ -55,8 +52,8 @@ public class Customer {
                 frequentRenterPoints++;
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         //add footer lines
